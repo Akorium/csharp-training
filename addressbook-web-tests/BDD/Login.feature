ce436724b31e0327ac7abb33241636b6857a8f34
Akorium <komlevvvanya72@gmail.com>
2023-05-30 18:55:44 +0700

    Добавлен SpecFlow

---
+++ addressbook-web-tests/BDD/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+
+Scenario: Login with valid credentials
+	Given A user is logged out
+	When I login with valid credentials
+	Then I have logged in
+
+Scenario: Login with invalid credentials
+	Given A user is logged out
+	When I login with valid credentials
+	Then I have not logged in
---
+++ addressbook-web-tests/BDD/Login.feature.cs
@@ -0,0 +1,11 @@
+﻿#error Error during generation. 
+//Command executed:
+//  C:\Users\vanya\Source\Repos\addressbook-web-tests\packages\SpecFlow.1.4.0\tools> C:\Users\vanya\AppData\Local\Microsoft\VisualStudio\17.0_a771bce4\Extensions\xrybhv12.lqz\Connectors\V1\specflow-vs.exe generate C:\Users\vanya\Source\Repos\addressbook-web-tests\addressbook-web-tests\BDD\Login.feature C:\Users\vanya\Source\Repos\addressbook-web-tests\addressbook-web-tests\App.config .cs addressbook_web_tests.BDD C:\Users\vanya\Source\Repos\addressbook-web-tests\addressbook-web-tests\ ""
+//Exit code: 4
+//Message: 
+//Error: Не удалось загрузить тип "TechTalk.SpecFlow.Generator.Interfaces.ITestGenerator" из сборки "TechTalk.SpecFlow.Generator, Version=1.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41".
+//Exception: System.TypeLoadException
+//StackTrace:
+//   в SpecFlow.VisualStudio.SpecFlowConnector.Generation.BaseGenerator.Generate(String projectFolder, String configFilePath, String targetExtension, String featureFilePath, String targetNamespace, String projectDefaultNamespace, Boolean saveResultToFile)
+//   в SpecFlow.VisualStudio.SpecFlowConnector.Generation.GenerationProcessor.Process() в D:\a\1\s\Connectors\SpecFlow.VisualStudio.SpecFlowConnector.V1\Generation\GenerationProcessor.cs:строка 41
+//   в SpecFlow.VisualStudio.SpecFlowConnector.ConsoleRunner.EntryPoint(String[] args) в D:\a\1\s\Connectors\SpecFlow.VisualStudio.SpecFlowConnector\ConsoleRunner.cs:строка 34
